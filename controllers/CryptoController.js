86263f82a87a791a4ce00c61501bcfdfdd34cc4a
Jason Ouyang <jasonouyang8000@gmail.com>
2021-06-08 07:40:53 -0700

    crypto history route

--- controllers/CryptoController.js
+++ controllers/CryptoController.js
@@ -4,8 +4,6 @@ const cryptoController = {};
 
 
 cryptoController.searchCrypto = async (req,res) => {
-
-
     try {
         const response = await axios.get(`https://api.coinranking.com/v2/search-suggestions?query=${req.query.search}`, {
             headers: {
@@ -24,7 +22,30 @@ cryptoController.searchCrypto = async (req,res) => {
 
 
 cryptoController.getCryptoHistory = async (req, res) => {
-    
+    try {
+        if (!req.query.uuid || !req.query.time ) {
+            return res.status(400).json({
+                error: 'One or more parameters are missing'
+            });
+        }
+
+
+        const response = await axios.get(`https://api.coinranking.com/v2/coin/${req.query.uuid}/history?timePeriod=${req.query.time}`, {
+            headers: {
+                'x-access-token': process.env.API_KEY
+            }
+        });
+
+        return res.json({results: response.data});
+    }
+
+    catch(error) {
+        return res.status(400).json({
+            error
+        })
+    }
 }
+
+
 
 module.exports = cryptoController;
--- routers/CryptoRouter.js
+++ routers/CryptoRouter.js
@@ -4,6 +4,8 @@ const cryptoRouter = require('express').Router();
 
 
 cryptoRouter.get('/', cryptoController.searchCrypto);
+cryptoRouter.get('/history',cryptoController.getCryptoHistory);
+
 
 
 module.exports = cryptoRouter;
